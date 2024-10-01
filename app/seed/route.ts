7f9cf93f11089086a27e74ceb3e214909749e830
tborjal <bor22014@byui.edu>
2024-10-01 16:59:22 +0800

    "Remove default response and uncomment seeding logic in route.ts"

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -102,10 +102,6 @@ async function seedRevenue() {
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
   try {
     await client.sql`BEGIN`;
     await seedUsers();
@@ -117,6 +113,6 @@ export async function GET() {
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
     await client.sql`ROLLBACK`;
-    return Response.json({ error }, { status: 500 });
+    return Response.json({ error }, { status: 500 }); 
   }
 }
