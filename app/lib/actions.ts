f135640ee1eafdf9e7e229f0e0934e42c870f910
tborjal <bor22014@byui.edu>
2024-10-02 20:27:27 +0800

    added error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -70,7 +70,6 @@ export async function updateInvoice(id: string, formData: FormData) {
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
-
   export async function deleteInvoice(id: string) {
     try {
         await sql`DELETE FROM invoices WHERE id = ${id}`;
