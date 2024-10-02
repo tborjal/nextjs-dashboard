d35bf4e333b77b96122eaae1e8f32e78d6b7a4a8
tborjal <bor22014@byui.edu>
2024-10-02 23:21:01 +0800

    more configurations

--- next.config.mjs
+++ next.config.mjs
@@ -5,4 +5,10 @@ const nextConfig = {
       ppr: 'incremental',
     },
   };
+  module.exports = {
+    eslint: {
+      ignoreDuringBuilds: true, // This will ignore ESLint errors during the build
+    },
+    // other configurations can go here...
+  };
   export default nextConfig;
