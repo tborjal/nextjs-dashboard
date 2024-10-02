a3b419454fa161232a9b3e2164a91781912b12a9
tborjal <bor22014@byui.edu>
2024-10-02 22:49:41 +0800

    configured issues and errors

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,5 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { updateInvoice } from '@/app/lib/actions';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
  
